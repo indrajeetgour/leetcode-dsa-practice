2f991f17019c0749d8495def5bbb4f1c9c4d0633
Indrajeet Gour <ijgour@gmail.com>
2022-08-22 02:08:24 +0530

    Update 121-best-time-to-buy-and-sell-stock.py

--- neetcode/121-best-time-to-buy-and-sell-stock.py
+++ neetcode/121-best-time-to-buy-and-sell-stock.py
@@ -38,3 +38,14 @@ class Solution:
             index +=1
         return max_profit
         
+## second way - need to understand more 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+
+        l = 0
+        for r in range(1, len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            res = max(res, prices[r] - prices[l])
+        return res
